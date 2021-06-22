76720c70ece1faff7d61c066c6f3f1c11de5696f
Carlos <Carlos@xtrene.com>
2021-06-22 17:53:42 +0200

    Agregar archivos de proyecto.

---
+++ Tarea04_22/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tarea04_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pass = "12345";
+            int intentos = 0;
+            const int limiteIntentos = 3;
+            string entrada = "";
+
+            while (intentos < limiteIntentos && entrada != pass)
+            {
+                Console.Write("Introduce la contraseña (intento {0}/{1}): ", intentos+1, limiteIntentos);
+                entrada= Console.ReadLine();
+                if (entrada == pass)
+                {
+                    Console.WriteLine("ENHORABUENA !!! Contraseña correcta.");
+
+                }
+                else
+                {
+                    Console.WriteLine("¡¡¡ ERROR !!! Contraseña incorrecta.");
+                }
+                intentos++;
+
+            }
+
+
+
+            Console.WriteLine("FIN del programa.");
+            Console.ReadKey();
+
+        }
+    }
+}
